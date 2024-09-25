79c61f097a14979f117abd6ea4b7264b0d8926bb
Sandra Akafia <akafiasandra246@gmail.com>
2024-09-25 09:49:21 -1200

    initial commit

---
+++ Array 1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Array_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 2, 4, 6 }, { 1, 8, 9 }, { 10, 5, 3 } };
+            Console.WriteLine(numbers[2,0]);
+        }
+    }
+}
